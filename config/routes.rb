69659b6345c583f820a696ea50cfd166dc8c0d44
s-shiho <sou0921@fuji.waseda.jp>
2023-09-22 15:21:27 +0900

    削除機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root "home#top"
   resources :tests
 
-  resources :posts, only: [:new, :create, :show, :index] # 追加
+  resources :posts, only: [:new, :create, :show, :index, :destroy] # 追加
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
